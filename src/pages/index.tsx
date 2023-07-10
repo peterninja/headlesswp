1e5500a4de5e7408209b43f47303b9b28021065c
peterninja <peter@ninjatechnolabs.com>
2023-07-10 20:00:00 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,86 +57,86 @@ export default function Home({themeoptions,homepagedata}:any) {
     const counterData = homedata.counter;
     const topIndustrySlider = homedata.topIndustrySlider;
     const bottomIndustrySlider = homedata.bottomIndustrySlider;
-  return (
-    <>
-      <Header themeoptions={themeoptions}/>
-        <section className="hero-banner">
-            <div className="container">
-                <div className="row justify-content-md-between justify-content-center align-items-center">
-                    <div className="col-md-10" data-aos="fade" data-aos-delay="50">
-                        <h1>
-                            {homedata.textLine1 && (
-                                <span className="overflow-hidden d-inline-block">
-                                    <span className="anim-gsap-1 d-inline-block">{homedata.textLine1}</span>
-                                </span> 
-                            )}
-                            {homedata.textLine2 && (
-                                <span className="overflow-hidden d-inline-block title-one pb-1">
-                                    <span className="overflow-hidden text-ani-one">{homedata.textLine2}</span> 
-                                </span>
-                            )}
-                            {homedata.textLine3 && (
-                                <span className="overflow-hidden d-inline-block">
-                                    <span className="anim-gsap-2 d-inline-block">{homedata.textLine3}</span>
-                                </span>
-                            )}
-                            {homedata.textLine4 && (
-                                <span className="overflow-hidden d-inline-block">
-                                    <span className="anim-gsap-3 d-inline-block">{homedata.textLine4}</span>
-                                </span>
-                            )}
-                            {homedata.textLine5 && (
-                                <span className="overflow-hidden d-block">
-                                    <span className="anim-gsap-4 d-inline-block">
-                                        <strong><span id="changingword">{homedata.textLine5}</span></strong>
+    return (
+        <>
+        <Header themeoptions={themeoptions}/>
+            <section className="hero-banner">
+                <div className="container">
+                    <div className="row justify-content-md-between justify-content-center align-items-center">
+                        <div className="col-md-10" data-aos="fade" data-aos-delay="50">
+                            <h1>
+                                {homedata.textLine1 && (
+                                    <span className="overflow-hidden d-inline-block">
+                                        <span className="anim-gsap-1 d-inline-block">{homedata.textLine1}</span>
+                                    </span> 
+                                )}
+                                {homedata.textLine2 && (
+                                    <span className="overflow-hidden d-inline-block title-one pb-1">
+                                        <span className="overflow-hidden text-ani-one">{homedata.textLine2}</span> 
                                     </span>
-                                </span>
-                            )}
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section>
-        <div className="container">
-            <div className="row">
-                <div className="col-xl-9 col-lg-8">
-                    <div className="who-we-info secondary-light-bg">
-                        <h2 data-aos="fade-up">{homedata.wHeading}</h2>
-                        <h3 data-aos="fade-up">{homedata.wSubHeading}</h3>
-                        <p dangerouslySetInnerHTML={{__html:homedata.wDescription}} />
-                    </div>
-                </div>
-                <div className="col-xl-3 col-lg-4">
-                    <div className="who-we-img wrapper-gsap" data-aos="fade" data-aos-delay="50">
-                        {homedata.wImage.mediaItemUrl &&(
-                            <img src={homedata.wImage.mediaItemUrl} alt=""/>
-                        )}
-                        {counterData &&(
-                        <div className="counters p-xl-4 p-md-3 p-2" id="counter">
-                            {counterData.map((counterItem: any,index: any) => {
-                            // console.log(index);
-                            return (
-                                <div className="counter-info" key={index}>
-                                    <div className="number"><span className="counter-value" data-count={counterItem.counterNumber}>{counterItem.counterNumber}</span>+</div>
-                                    <div className="counter-name">{counterItem.counterName}</div>
-                                </div>
-                                );
-                            })}
+                                )}
+                                {homedata.textLine3 && (
+                                    <span className="overflow-hidden d-inline-block">
+                                        <span className="anim-gsap-2 d-inline-block">{homedata.textLine3}</span>
+                                    </span>
+                                )}
+                                {homedata.textLine4 && (
+                                    <span className="overflow-hidden d-inline-block">
+                                        <span className="anim-gsap-3 d-inline-block">{homedata.textLine4}</span>
+                                    </span>
+                                )}
+                                {homedata.textLine5 && (
+                                    <span className="overflow-hidden d-block">
+                                        <span className="anim-gsap-4 d-inline-block">
+                                            <strong><span id="changingword">{homedata.textLine5}</span></strong>
+                                        </span>
+                                    </span>
+                                )}
+                            </h1>
                         </div>
-                        )}
+                    </div>
+                </div>
+            </section>
+            <section>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-9 col-lg-8">
+                        <div className="who-we-info secondary-light-bg">
+                            <h2 data-aos="fade-up">{homedata.wHeading}</h2>
+                            <h3 data-aos="fade-up">{homedata.wSubHeading}</h3>
+                            <p dangerouslySetInnerHTML={{__html:homedata.wDescription}} />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4">
+                        <div className="who-we-img wrapper-gsap" data-aos="fade" data-aos-delay="50">
+                            {homedata.wImage.mediaItemUrl &&(
+                                <img src={homedata.wImage.mediaItemUrl} alt=""/>
+                            )}
+                            {counterData &&(
+                            <div className="counters p-xl-4 p-md-3 p-2" id="counter">
+                                {counterData.map((counterItem: any,index: any) => {
+                                // console.log(index);
+                                return (
+                                    <div className="counter-info" key={index}>
+                                        <div className="number"><span className="counter-value" data-count={counterItem.counterNumber}>{counterItem.counterNumber}</span>+</div>
+                                        <div className="counter-name">{counterItem.counterName}</div>
+                                    </div>
+                                    );
+                                })}
+                            </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </section>
-        <section className="section-space pt-5 overflow-hidden">
-        <div className="container-fluid">
-          <div className="row text-center">
-            <div className="col-12 served-industries-outer">
-                <h2>{homedata.iHeading}</h2>
-                <div className="swiper-container industry-slide-top mt-xl-5 mt-4">
-                    <Swiper className="swiper-wrapper" {...settingsTopSlider}>
+            </section>
+            <section className="section-space pt-5 overflow-hidden">
+            <div className="container-fluid">
+            <div className="row text-center">
+                <div className="col-12 served-industries-outer">
+                    <h2>{homedata.iHeading}</h2>
+                    <div className="swiper-container industry-slide-top mt-xl-5 mt-4">
+                        <Swiper className="swiper-wrapper" {...settingsTopSlider}>
                             {topIndustrySlider.map((topsliderItem: any,index: any) => {
                                 return(
                                 <SwiperSlide>
@@ -146,26 +146,10 @@ export default function Home({themeoptions,homepagedata}:any) {
                                 </SwiperSlide>
                                 );
                             })}
-
-                        {/* <SwiperSlide>
-                            <a className="served-link" href="#">
-                                UI&UX
-                            </a>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <a className="served-link" href="#">
-                                BRANDING
-                            </a>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <a className="served-link" href="#">
-                                FRONT-END DEVELOPMENT
-                            </a>
-                        </SwiperSlide> */}
-                    </Swiper>
-                </div>
-                <div className="swiper-container industry-slide-bottom mt-4">
-                    <Swiper className="swiper-wrapper" {...settingsBottomSlider}>
+                        </Swiper>
+                    </div>
+                    <div className="swiper-container industry-slide-bottom mt-4">
+                        <Swiper className="swiper-wrapper" {...settingsBottomSlider}>
                             {bottomIndustrySlider.map((bottomsliderItem: any,index: any) => {
                                 return(
                                 <SwiperSlide>
@@ -175,30 +159,15 @@ export default function Home({themeoptions,homepagedata}:any) {
                                 </SwiperSlide>
                                 );
                             })}
-                        {/* <SwiperSlide>
-                            <a className="served-link" href="#">
-                                UI&UX
-                            </a>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <a className="served-link" href="#">
-                                BRANDING
-                            </a>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <a className="served-link" href="#">
-                                FRONT-END DEVELOPMENT
-                            </a>
-                        </SwiperSlide> */}
-                    </Swiper>
+                        </Swiper>
+                    </div>
                 </div>
             </div>
-          </div>
-        </div>
-        </section>
-        <Footer themeoptions={themeoptions}/>
-    </>
-  )
+            </div>
+            </section>
+            <Footer themeoptions={themeoptions}/>
+        </>
+    )
 }
 
 export async function getStaticProps() {
