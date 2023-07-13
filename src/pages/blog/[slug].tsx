ef1b9ef31f1acbce9a30b16883e5b0d5d7b868ec
peterninja <peter@ninjatechnolabs.com>
2023-07-13 19:53:35 +0530

    blog detail page created

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,21 +1,6 @@
-import { useRouter } from "next/router";
-import GraphAPI from "@/service/graphQL";
-import { useEffect } from "react";
-
-
 function BlogDetails() {
     return (
       <h3>Blog Detail</h3>
     );
   }
   export default BlogDetails;
-
-  export async function getStaticPaths() {
-    const blogPosts = await GraphAPI.blogPostListing();
-    const paths = blogPosts.data.data.posts.edges.map((item) => {
-      const params = { slug: item.node.slug };
-      return { params };
-    });
-    return { paths, fallback: true };
-  }
-  
